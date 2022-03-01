02b58d87403ed42530e521e71d4afdcf392dea3d
Jamie Bunch <jamiebunch@Jamies-MacBook-Air.local>
2022-03-01 11:13:59 -0500

    update front-end

---
+++ CODE/auth-web/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import Signup from './scenes/Signup';
+
+function App() {
+  const [token, setToken] = useState();
+  const [isUser, setIsUser] = useState();
+  return (
+  <section>
+    {!token
+        ? isUser
+          ? <h1>Login</h1>
+          : <Signup setToken={setToken} setIsUser={setIsUser} />
+        : <h1>User List</h1>
+    }
+  </section>
+  );
+}
+
+export default App;
