be6806e9aab56e44c3ef157e7de6554d3c6be735
Jamie Bunch <JamieBunch@ymail.com>
2022-03-15 11:29:40 -0400

    added componets

---
+++ CODE/final-app/src/App.js
@@ -0,0 +1,50 @@
+import { Routes, Route } from 'react-router-dom'
+import { Layout } from 'antd'
+import Home from './scenes/Home';
+const { Header, Footer, Content } = Layout
+
+const styles = {
+  header: {
+    // position: "fixed",
+    // zIndex: 0,
+    width: "100%",
+    // color: "white",
+    padding: "0 12px"
+  },
+
+  // h1: {
+  //   color: 'white',
+  //   padding: '0 0px 0px',
+  // },
+
+  content: {
+    padding: "0 50px",
+    marginTop: 64,
+  },
+
+  footer: {
+    width: "100%",
+    color: "#001529",
+    padding: "0 12px"
+  }
+};
+
+export default function App() {
+  return (
+    <>
+      <Layout color="grey">
+        <Header style={styles.header}>
+          <h1 style={{ color: "lime", padding: "0 0px 0px" }}>easyFi</h1>
+        </Header>
+        <Content>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            {/* <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/create" element={<CreateStrategy />} /> */}
+          </Routes>
+        </Content>
+        <Footer></Footer>
+      </Layout>
+    </>
+  );
+}
---
+++ CODE/final-app/src/scenes/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hero from '../components/home/Hero'
+import Main from '../components/home/Main'
+import Footer from '../components/common/Footer';
+
+
+export default function Home() {
+
+    return (
+        <>
+        {/* <Navbar /> */}
+        <Hero />
+        <Main />
+        <Footer />
+        </>
+    )
+}
